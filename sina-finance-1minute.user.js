bd9dc2651c08115f8edd3ca86ec7564c9717e573
lgh06 <hnnk@qq.com>
2017-01-24 18:14:04 +0800

    add greasemonkey user js

---
+++ sina-finance-1minute.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         sina-finance-1minitue
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @match        http://finance.sina.com.cn/h5charts/tchart.html*
+// @grant        none
+// @author       lgh06
+// @require      https://lib.sinaapp.com/js/jquery/2.2.4/jquery-2.2.4.min.js
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if(window.opener) return;
+    var $ = jQuery.noConflict();
+    var year = 2008; //you can modify year.
+    var month = 1; //do not modify this.
+    var date = 1;//do not modify this.
+    var url = (y,m,d)=> {
+        y = y || year;
+        m = m || month;
+        d = d || date;
+        return `http://finance.sina.com.cn/h5charts/tchart.html?symbol=sh600103&date=${y}-${m}-${d}&rangeselector=true&indicator=tvol`;
+    };
+    console.log(window.parent,window.opener);
+    var time = 0;
+    setInterval(()=>{
+        time++;
+        if(year >= 2017 && month >= 1 && date>=2) return;
+
+        date = 2;
+
+        if(month == 13) month = 1;
+        if(time!=1 && (time-1) % 12 === 0)  year++;
+        var c = window.open(url());
+        setTimeout(()=>{
+            c.close();
+        },5000);
+        console.log(year,month,date);
+        month++;///
+    },6000);
+})();
